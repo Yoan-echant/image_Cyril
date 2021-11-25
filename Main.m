ae108c02642e0f444c51e44efc70bbd73b411110
Yoan Echantillon <yoan.echantillon@enseirb-matmeca.fr>
2021-11-25 12:24:53 +0100

    Homotétie presque bon

--- Main.m
+++ Main.m
@@ -2,10 +2,9 @@ clc;
 clear;
 close all;
 
-img1=imread('Img/');
-img2=imread('Img/');
+img1=imread('Img/monument.bmp');
+img2=imread('Img/monument_ext.bmp');
 
 h=find_homotetie_man(img2,img1);
-%newim=homotetie(img2,h);
 
 %mosaique=create_mos(img1,newim);
--- find_homotetie_man.m
+++ find_homotetie_man.m
@@ -7,11 +7,17 @@ function [h]=find_homotetie_man(img1,img2)
     imshow(img2)
     [X2,Y2]=ginput(4);
 
+    A=zeros(8,8);
+    B=zeros(8,1);
+    for i=0:3
+        A(2*i+1,:)=[X1(i+1) Y1(i+1) 1 0 0 0 -X1(i+1)*X2(i+1) -Y1(i+1)*X2(i+1)];
+        A(2*i+2,:)= [0 0 0 X1(i+1) Y1(i+1) 1 X1(i+1)*Y2(i+1) -Y1(i+1)*Y2(i+1)];
+        B(2*i+1)=X2(i+1);
+        B(2*i+2)=Y2(i+1);
+    end
+        
     
-    disp('taille matrice')
-    disp(size(A))
-    
-    h=
+    h=(1/(transpose(A)*A))*transpose(A)*B;
     
     disp('la matrice homotetie est ')
     disp(h)
