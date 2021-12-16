75dd4cfb233a8a547836f4506ec794c7618a6981
cyrH59 <channier@enseirb-matmeca.fr>
2021-12-16 09:35:44 +0100

    modif

--- Main.m
+++ Main.m
@@ -16,17 +16,20 @@ imshow(img1)
 [h]=find_homographie_man(X1,Y1,X2,Y2);
 [imgtransforme,mask]=application_homographie2(img2,h,img1);
 
-%[h]=find_homotetie_man(img2,img1);
-
-%mosaique=create_mos(img1,img2,h);
-%figure;
-%imshow(mosaique);
-
 % nouvelle image homographie 
 figure;
 imshow(uint8(imgtransforme));
 figure;
 imagesc(mask);
+
+% extraction image : 
+hinv=inv(h);
+[imgtransformebis,maskbis]=application_homographie2(img2,hinv,img1);
+figure;
+imshow(uint8(imgtransformebis));
+figure;
+imagesc(maskbis);
+
 
 % supoerposition image :
 %{
@@ -52,4 +55,18 @@ title("image superposée");
 % extrêmes, regarder min et max de chaque extremités pour avoir la taille
 % de la nouvelle image 
 % attention les coordonnées sont négatives
+
+% fonction 1 : estimation homographie + projection + extraction
+% fonction 2 : triplet I,M,B (hauteur largueur)
+% fonction 3 : Application homographie à un triplet
+% fonction 4 : fusion, diviser I3 par le masque, sommer I1 et I2 
+
+% Il faut calculer les matrices d'homographies entre chaque image, il faut
+% choisir un univers d'arrivée ensuite. 
+% il faut ramener l'univers de la photo 2 vers la photo 1 si on suppose que
+% l'univers de départ est 1
+% On fait donc H12-1 pour ramener 2 vers 1
+% Pour ramener 3 vers 1 on fait (H23H12)-1. On applique fonction 4. On fait
+% une cascade d'appel. 
+
 
--- find_homographie_man.m
+++ find_homographie_man.m
@@ -4,7 +4,7 @@ function [h_fin]=find_homographie_man(X1,Y1,X2,Y2)
     B=zeros(8,1);
     for i=0:3
         A(2*i+1,:)=[X1(i+1) Y1(i+1) 1 0 0 0 -X1(i+1)*X2(i+1) -Y1(i+1)*X2(i+1)];
-        A(2*i+2,:)= [0 0 0 X1(i+1) Y1(i+1) 1 X1(i+1)*Y2(i+1) -Y1(i+1)*Y2(i+1)];
+        A(2*i+2,:)= [0 0 0 X1(i+1) Y1(i+1) 1 -X1(i+1)*Y2(i+1) -Y1(i+1)*Y2(i+1)];
         B(2*i+1)=X2(i+1);
         B(2*i+2)=Y2(i+1);
     end
@@ -15,7 +15,9 @@ function [h_fin]=find_homographie_man(X1,Y1,X2,Y2)
     h_fin(1,:)=h(1:3);
     h_fin(2,:)=h(4:6);
     h_fin(3,1:2)=h(7:8);
-    hfin=transpose
+  
+
+end
     
 
 
