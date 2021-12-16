4bdb41652f9e0aaac26fc6d2a8ee8463e1fc436a
Yoan Echantillon <yoan.echantillon@enseirb-matmeca.fr>
2021-12-16 09:37:50 +0100

    Bon d'accord je push mais c'est bien parceque t'insiste

--- application_homographie2.m
+++ application_homographie2.m
@@ -1,19 +1,17 @@
 function[imgtransforme, mask]=application_homographie2(img2,hmat,img1)
-hmat=eye(3)/hmat;
+hmat=inv(hmat);
 [h,w,z ]=size(img1);
 [h2,w2,z2]=size(img2);
-imgtransforme=zeros(h,w,z);
+imgtransforme=img1;
 mask=zeros(h,w);
 
 for i=1:h
     for j=1:w
-        x=floor((hmat(1,1)*j+hmat(1,2)*i+hmat(1,3))/(hmat(3,1)*j+hmat(3,2)*i+hmat(3,3)));
-        y=floor((hmat(2,1)*j+hmat(2,2)*i+hmat(2,3))/(hmat(3,1)*j+hmat(3,2)*i+hmat(3,3)));
+        y=floor((hmat(1,1)*j+hmat(1,2)*i+hmat(1,3))/(hmat(3,1)*j+hmat(3,2)*i+hmat(3,3)));
+        x=floor((hmat(2,1)*j+hmat(2,2)*i+hmat(2,3))/(hmat(3,1)*j+hmat(3,2)*i+hmat(3,3)));
         if (x>0 && x<h2+1 && y>0 && y<w2+1)
             imgtransforme(i,j,:)=img2(x,y,:);
             mask(i,j)=1;
-        else
-            imgtransforme(i,j,:)=img1(i,j,:);
         end
     end
 end
